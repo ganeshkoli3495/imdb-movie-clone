3d9af0cacfd67d0cac5f87b579917498d4dee3ab
ganeshkoli3495 <ganeshgeaxy@gmail.com>
2022-12-26 17:17:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import MovieList from "./components/MovieList/MovieList";
+import Movie from "./pages/MovieDetail/Movie";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route
+            path="movies/:type"
+            element={
+              <h1>
+                <MovieList />
+              </h1>
+            }
+          ></Route>
+          <Route path="/*" element={<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { Link } from "react-router-dom";
+import MovieList from "../../components/MovieList/MovieList";
+
+const Home = () => {
+  const apikey = process.env.REACT_APP_API_KEY;
+  const [popularMovies, setPopularMoves] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setPopularMoves(data.results));
+  }, []);
+  return (
+    <div className="poster">
+      <Carousel
+        showThumbs={false}
+        autoPlay={true}
+        transitionTime={3}
+        infiniteLoop={true}
+        showStatus={false}
+      >
+        {popularMovies.map((movie, index) => (
+          <Link
+            style={{ textDecoration: "none", color: "white" }}
+            to={`/movie/${movie.id}`}
+          >
+            <div className="posterimage" key={index}>
+              <img
+                src={`https://image.tmdb.org/t/p/original${
+                  movie && movie.backdrop_path
+                }`}
+              />
+              <div className="posterimage__overlay">
+                <div className="posterimage__title">
+                  {movie ? movie.original_title : " "}
+                </div>
+                <div className="posterimage__runtime">
+                  {movie ? movie.release_date : ""}
+                  <span className="posterimage__rating">
+                    {movie ? movie.vote_average : ""}
+                    <i className="fas fa-star" />
+                  </span>
+                </div>
+                <div className="posterimage__description">
+                  {movie ? movie.overview : ""}
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </Carousel>
+      <MovieList />
+    </div>
+  );
+};
+
+export default Home;
